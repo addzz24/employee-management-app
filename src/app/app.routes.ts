7c900f699453accb73974832dc6cccd5d7cd62db
addzz24 <adityabhosale375@gmail.com>
2026-04-06 20:51:09 +0530

    1. scss base setup 2. custom font added 3. layout and feature components added 4. routing for features added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('../app/features/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+      },
+      {
+        path: 'employee-list',
+        loadComponent: () =>
+          import('../app/features/employee-list/employee-list.component').then(
+            (m) => m.EmployeeListComponent,
+          ),
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  router = inject(Router);
+
+  onNavItemClick(path: string) {
+    this.router.navigate([`/${path}`]);
+  }
+}
