b0fc5d6fab2f550d494f73253e845c36a96406fb
addzz24 <adityabhosale375@gmail.com>
2026-04-07 18:20:21 +0530

    minror changes

--- src/app/shared/components/data-table/data-table.component.ts
+++ src/app/shared/components/data-table/data-table.component.ts
@@ -11,13 +11,11 @@ import { FormsModule } from '@angular/forms';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatSortModule, MatSort } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { TableAction, TableActionEvent } from '../../../core/types/types';
 import { TitleCaseSplitPipe } from '../../../core/pipes/title-case-split.pipe';
-import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-data-table',
@@ -26,14 +24,11 @@ import { MatChipsModule } from '@angular/material/chips';
     CommonModule,
     MatTableModule,
     MatSortModule,
-    MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    FormsModule,
     MatIconModule,
     MatTooltipModule,
     TitleCaseSplitPipe,
-    MatChipsModule,
   ],
   templateUrl: './data-table.component.html',
 })
